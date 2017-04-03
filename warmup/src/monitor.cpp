6b64cc2b7e35272966fe84e3f4bc675134685167
lzuehsow <lydiazuehsow@gmail.com>
2017-04-03 01:31:46 -0400

    Pushing working vers. of code. Still too laggy to work with warmup_node.

--- warmup/src/monitor.cpp
+++ warmup/src/monitor.cpp
@@ -3,18 +3,27 @@
 #include <iostream>
 #include <boost/thread/thread.hpp>
 
+// void run_databags(){
+// /*Plays a rosbag on loop, while terminal output is silenced to prevent blocking the cout channel.*/
+
+// 	std::cout << std::endl << "Playing rosbag lydia1.bag." << std::endl;
+// 	std::system("rosbag play ~/catkin_ws/src/bravobot/warmup/rosbags/lydia1.bag -q"); 
+// }
+
 void load_programs(int passed_ran_calib, int passed_ran_track) {
+	/*Executes system commands that run rosnodes one at a time, according to set 'ran_X' parameters in main function.*/
 
 	if (passed_ran_calib == 0){
-		std::cout << std::endl << "Talker node booting up." << std::endl;
-		std::system("rosrun talker talker");
-	}
-	if (passed_ran_track == 0){
 		std::cout << std::endl << "Listener node booting up." << std::endl;
 		std::system("rosrun listener listener");
 	}
 
-	std::cout << std::endl << "Program complete." << std::endl;
+	if (passed_ran_track == 0){
+		std::cout << std::endl << "Talker node booting up." << std::endl;
+		std::system("rosrun talker talker");
+	}
+
+	std::cout << std::endl << "All programs executed." << std::endl;
 
 //WORKING CODE
  //    while(true){
@@ -26,24 +35,36 @@ void load_programs(int passed_ran_calib, int passed_ran_track) {
 }
 
 int main(){
+
 	int ran_calib = 0; //Did not run calibration
 	int ran_track = 0; //Did not run tracking
 
-	boost::thread t(load_programs,ran_calib, ran_track);
+	// boost::thread rosbags(run_databags);
+
+	boost::thread programs(load_programs,ran_calib, ran_track);
+
+	std::cout << std::endl << "Running main program thread." << std::endl;
 
     // Main thread waits for "Enter" keypress.
     while (true){
+    	// std::cout << std::endl << "DATA: " << std::cin.get() << std::endl; //Here for debug
+
 	    if (std::cin.get() == '\n'){
-			std::cout << std::endl << "Talker node shutting down now." << std::endl;
-		    std::system("rosnode kill talker talker");
+			std::cout << std::endl << "Listener node shutting down." << std::endl;
+		    std::system("rosnode kill listener listener");
+		    ran_calib = 1; // Ran calibration
 		}
 		if (std::cin.get() == '\n'){
-			std::cout << std::endl << "Listener node shutting down now." << std::endl;
-		    std::system("rosnode kill listener listener");
+			std::cout << std::endl << "Talker node shutting down." << std::endl;
+		    std::system("rosnode kill talker talker");
+		    ran_track = 1; // Ran tracking software
+		}
+		if (ran_calib == 1 && ran_track == 1){
+			std::cout << std::endl << "Program completed!" << std::endl;
+			break;
 		}
 	}
 
-	ran_calib = 1; // Set the boolean to true. The loop thread will exit from the loop and terminate.
 	return 0;
 
 }
