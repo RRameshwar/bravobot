f1992edb60bc20e72a2be12cab8ea4af5d851bac
ashiqali21 <ashiqali.mohamedkassim@students.olin.edu>
2017-04-25 20:44:36 -0400

    PersonMonitor algorithm implemented and publishes to personmonitor_pub_

---
+++ warmup/src/person_monitor.cpp
@@ -0,0 +1,88 @@
+#include <cmath>
+
+#include <ros/ros.h>
+#include <tf/transform_listener.h>
+
+class Monitor{
+
+    double dx;
+    double dy;
+    double distance;
+public:
+
+    ros::NodeHandle nh_;
+
+    ros::Publisher personmonitor_pub_;
+    tf::TransformListener listener;
+    tf::StampedTransform current_transform;
+    tf::StampedTransform old_transform;
+
+    bool tf_success;
+
+    Monitor()
+    {
+        personmonitor_pub_ = nh_.advertise<std_msgs::Bool>("stop", 10);
+    }
+
+    void MonitorPerson(){
+
+        tf_success = true;
+
+        try{
+            listener.lookupTransform("/person", "/map",
+                                     ros::Time(0), current_transform);
+        }
+        catch (tf::TransformException ex){
+            ROS_ERROR("%s",ex.what());
+            ros::Duration(1.0).sleep();
+            tf_success = false;
+        }
+
+        try{
+            listener.lookupTransform("/person", "/map",
+                                     ros::Time(0)-ros::Duration(5), old_transform);
+        }
+        catch (tf::TransformException ex){
+            ROS_ERROR("%s",ex.what());
+            ros::Duration(1.0).sleep();
+            tf_success = false;
+        }
+
+        if (!tf_success) {
+            personstopped(false);
+            return;
+        }
+
+        dx = old_transform.getOrigin().x() - current_transform.getOrigin().x();
+        dy = old_transform.getOrigin().y() - current_transform.getOrigin().y();
+        distance = sqrt(pow(dx,2)+pow(dy,2));
+        if(distance<0.2){   //if person moved less than 0.2 metres
+            personstopped(true);
+        }
+        else{
+            personstopped(false);
+        }
+        return;
+    }
+
+    void personstopped(bool msg){
+        if(msg){
+            std::cout << "person stopped moving" << std::endl;
+        }
+        personmonitor_pub_.publish(msg);
+    }
+};
+
+
+int main(int argc, char** argv) {
+    ros::init(argc, argv, "transform_listener");
+    Monitor pm;
+
+    ros::Rate rate(10.0);
+    while (pm.nh_.ok()){
+        pm.MonitorPerson();
+        rate.sleep();
+    }
+
+    return 0;
+}
